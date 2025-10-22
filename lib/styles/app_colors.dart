09c6d87a8c50bf77ceca99ef830a39a91a06fe54
liyixin <1367730617@qq.com>
2025-10-22 16:56:07 +0800

    添加自定义样式类

---
+++ lib/styles/app_colors.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color blackColor1 = Color(0xFFFFFFFF);
+  static const Color blackColor2 = Color(0xFFF5F5F5);
+  static const Color blackColor3 = Color(0xFFEEEEEE);
+  static const Color blackColor4 = Color(0xFFE0E0E0);
+  static const Color blackColor5 = Color(0xFFBDBDBD);
+
+  static const Color unblackColor1 = Color(0xFF121212);
+  static const Color unblackColor2 = Color(0xFF1E1E1E);
+  static const Color unblackColor3 = Color(0xFF242424);
+  static const Color unblackColor4 = Color(0xFF2D2D2D);
+  static const Color unblackColor5 = Color(0xFF3D3D3D);
+
+  static const Color blackColor333 = Color(0xFF111827);
+  static const Color unblackColor333 = Color(0xFF4B5563);
+
+  //按钮红色背景
+  static const redBtnColor = Color(0xFFFF3B3D);
+  static const cursorColor = Color(0xFFE9AAAB);
+
+  //按钮/标题黑色
+  static const titleColor3030 = Color(0xFF303030);
+
+  //黑色标题
+  static const blackTitleColor = Color(0xFF2b2b2b);
+
+  //搜索框背景颜色
+  static const searchBgColor = Color(0xFFf7f7f7);
+
+  //透明度80的白色
+  static const whiteColor80 = Color(0x80FFFFFF);
+
+  //分割线灰色
+  static const verticalLimitColor = Color(0xFFd1d1d1);
+  static const lineColor = Color(0xFFf5f4f4);
+  static const lineColorF8 = Color(0xFFf8f8f8);
+  static const lineColorFA = Color(0xFFfafafa);
+
+  //文本颜色
+  static const textColor3a = Color(0xFF3a3a3a);
+  static const textColor4a = Color(0xFF4a4a4a);
+  static const textColor4D = Color(0xFF4d4d4d);
+  static const textColor33 = Color(0xFF333333);
+  static const textColor5a = Color(0xFF5a5a5a);
+  static const textColor5e = Color(0xFF5e5e5e);
+  static const textColor78 = Color(0xFF787878);
+  static const textColor7d = Color(0xFF7d7d7d);
+  static const textColorAd = Color(0xFFadadad);
+  static const textColorA9 = Color(0xFF7ee2a9);
+  static const textColor86 = Color(0xFF868686);
+  static const textColor8B = Color(0xFF8b8b8b);
+  static const textColorB8 = Color(0xFFB8B8B8);
+  static const textColor2b = Color(0xFF2b2b2b);
+  static const textColor20 = Color(0xFF202020);
+  static const textColorB6 = Color(0xFFb6b6b6);
+  static const textColor41 = Color(0xFF424241);
+  static const textColor7A = Color(0xFF7a7a7a);
+  static const textColor7C = Color(0xFF7c7c7c);
+  static const textColorC0 = Color(0xFFc1c1c0);
+  static const textColor9A = Color(0xFF9c9c9a);
+  static const textColorB7 = Color(0xFFb7b7b7);
+  static const textColor40 = Color(0xFF404040);
+  static const textColor59 = Color(0xFF595959);
+  static const textColorF6 = Color(0xFFf6f6f6);
+  static const textColor62 = Color(0xFFfb6c62);
+  static const textColorAE = Color(0xFFb0b0ae);
+  static const textRedColor39 = Color(0xFFff3739);
+  static const textRedColor6d = Color(0xFFff6c6d);
+  static const textRedColor3D = Color(0xFFff3b3d);
+  static const textRedColor30 = Color(0xFFff2e30);
+  static const textColor7c = Color(0xFFfbac7c);
+  static const collectColor = Color(0xffef742a);
+
+
+  //   文本背景色
+  static const textBgColorFF = Color(0xffffffff);
+}
